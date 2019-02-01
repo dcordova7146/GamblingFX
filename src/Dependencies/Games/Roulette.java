f415e236af1cd2b22d54aadcc4ee7444cb4e20cd
dego <dcordova7146@bths.edu>
2019-02-01 10:14:32 -0500

    im mega stoopid still planning

--- src/Dependencies/Games/Roulette.java
+++ src/Dependencies/Games/Roulette.java
@@ -1,4 +1,21 @@
 package Dependencies.Games;
 
-public class Roulette {
+import Dependencies.Systems.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Roulette game
+ * @Authors Diego Cordova & Nick Chen
+ */
+
+public class Roulette extends GamblingGame {
+    private int redPot = 0;
+    private int blackPot = 0;
+    private int greenPot = 0;
+
+
+
+
 }
