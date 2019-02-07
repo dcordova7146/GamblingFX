94dd8b4cf6193008708d757504f525df7a5224af
dego <dcordova7146@bths.edu>
2019-02-07 09:17:43 -0500

    done?

--- src/Dependencies/Games/Roulette.java
+++ src/Dependencies/Games/Roulette.java
@@ -14,52 +14,37 @@ import java.util.HashMap;
 //change place bet there is no pot just multiplies your bet if you win or just get nothing
 
 public class Roulette extends GamblingGame {
-    private int redPot = 0;
-    private int blackPot = 0;
-    private int greenPot = 0;
-    private String[] wheel = new String[15];
 
-    public void placeBet(int betAmount, String bet, User player){
-        if(bet.equals("green")){
-            greenPot += betAmount;
-        }
-        if(bet.equals("red")){
-            redPot += betAmount;
-        }
-        if(bet.equals("black")){
-            blackPot += betAmount;
-        }
+    public void placeBet(int betAmount, User player){
+        userManager.updatePlayerBalance(player, -(betAmount));
     }
 
-    public void generateWheel(String[] wheel){
-        for(int i = 0;i<wheel.length;i++){
-            wheel[i] = randomColor();
+    public String[] generateWheel(){
+        String[] wheel = new String[15];
+        wheel[0] = "green";
+        for(int i = 1;i<wheel.length;i++){
+            if(i >8) {
+                wheel[i] = "black";
+            }else {
+                wheel[i] = "red";
+            }
         }
-    }
-
-    public String randomColor(){
-        int rCounter = 0;
-        int bCounter = 0;
-        if(bCounter == 7 && rCounter == 7){
-            return "green";
-        }
-        if((int)(Math.random() * 100) > 50){
-            rCounter++;
-            return "red";
-        }else {
-            bCounter++;
-            return "black";
-        }
+        return wheel;
     }
 
     public String calculateResult(){
 
         if(((int)Math.random() *100) <= 55){
             return "black";
-        }else if(((int)Math.random() *100) <= 99.7){
+        }else if(((int)Math.random() *100) <= 99){
             return "red";
         }else
             return "green";
+
+    }
+
+    public void updateWinnerBalance(String winningColor,User player, int winAMount){
+            userManager.updatePlayerBalance(player, winAMount);
 
     }
 
